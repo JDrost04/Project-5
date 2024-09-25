3dbb9bd8791346fb8335b3348d896c3d85294dc6
JDrost04 <533567@student.fontys.nl>
2024-09-25 11:59:22 +0200

    Een kolomnaam aangepast waardoor wiskundig model nu errorloos runt jippie

--- Wiskundige model.py
+++ Wiskundige model.py
@@ -16,7 +16,7 @@ Afstandsmatrix = all_sheets['Afstandsmatrix']
 max_cap = 300 # maximale capaciteit in kWH
 SOH = [85, 95] # State of Health
 oplaadtempo_90 = 450 / 60 # kwh per minuut bij opladen tot 90%
-oplaadtempo_10 = 60 /60 # kwh per minuut bij oladen tot 10%
+oplaadtempo_10 = 60 /60 # kwh per minuut bij opladen van 90% tot 100%
 DCap_85 = max_cap * 0.85 # (255 kWh)
 DCap_95 = max_cap * 0.95 # (285 kWh)
 DCap = [DCap_85, DCap_95]
@@ -41,14 +41,16 @@ Afstandsmatrix["min_speed"] = Afstandsmatrix["afstand in km"]/ Afstandsmatrix['m
 Afstandsmatrix["max_energy"] = Afstandsmatrix["afstand in km"]* 2.5
 Afstandsmatrix["min_energy"] = Afstandsmatrix["afstand in km"]* 0.7
 
-Dienstregeling['vertrektijd_dt'] = Dienstregeling['vertrektijd'].apply(lambda x: datetime.strptime(f'{int(x):02}:{int((x * 100) % 100):02}', '%H:%M')) # vertrijd omzetten naar datetime
+from datetime import datetime
+
+Dienstregeling['vertrektijd_dt'] = Dienstregeling['vertrektijd'].apply(lambda x: datetime.strptime(x, '%H:%M')) # vertrijd omzetten naar datetime
 def bereken_eindtijd(row):
     """ telt de maximale reistijd op bij de vertrektijd, zodat er een kolom komt met eindtijd
     Parameters: row
     Output: eindtijd in HH:MM
     """
     reistijd = Afstandsmatrix[(Afstandsmatrix['startlocatie'] == row['startlocatie']) & 
-                              (Afstandsmatrix['eindlocatie'] == row['eindlocatie'])]['min reistijd in min'].values
+                              (Afstandsmatrix['eindlocatie'] == row['eindlocatie'])]['min reistijd in uur'].values
     if len(reistijd) > 0:
         reistijd_in_min = reistijd[0] # Converteer reistijd naar minuten
         eindtijd = row['vertrektijd_dt'] + timedelta(minutes=reistijd_in_min)
@@ -128,11 +130,17 @@ def battery_usage(distance, huidige_tijd, vertrektijd, eindtijd, bus_type='high'
 """"   
 Overdag niet meer dan 90% opladen = 229,5 - 256,5 kWh -> staat in functie opladen 
 Altijd tenminste 15 min achtereen worden opgeladen. -> staat ook in opladen
-Veiligheidsmarge van ongeveer 10% van de SOH -> bus minimaal SOC (state of charge) waarde van 10%Veiligheidsmarge van ongeveer 10% van de SOH -> bus minimaal SOC (state of charge) waarde van 10%. Minimale hoeveelheid in accu = 25,5 -28,5 kWh -> staat ook in opladen
+Veiligheidsmarge van ongeveer 10% van de SOH -> bus minimaal SOC (state of charge) waarde van 10% 
+Veiligheidsmarge van ongeveer 10% van de SOH -> bus minimaal SOC (state of charge) waarde van 10%. 
+Minimale hoeveelheid in accu = 25,5 -28,5 kWh -> staat ook in opladen
 Verbruik per km =  0.7-2.5 kWh 
 Idling verbruikt het 0.01 kWh (verbruik van bus in stilstand) 
-Lijn 401 eerste rit vertrekt om 6:04 vanuit de airport (Apt) naar Eindhoven Centraal (bst). 401 laatste rit van Apt naar bst is om 00:31. Deze lijn zou klaar moeten zijn tussen de 00:53 en 00:56
-Lijn 401 eerste rit vertrekt om 5:07 vanuit Eindhoven Centraal (bst) naar airport (Apt). 401 laatste rit van bst naar apt is om 00:09. Deze lijn zou klaar moeten zijn tussen de 00:31 en 00:33. 
-Lijn 400 eerste rit vertrekt om 7:19 vanuit de airport (Apt) naar Eindhoven Centraal (bst). 400 laatste rit van Apt naar bst is om 20:46. Deze lijn zou klaar moeten zijn tussen de 21:07 en 21:09 
-Lijn 400 eerste rit vertrekt om 06:52 vanuit Eindhoven Centraal (bst) naar airport (Apt). 401 laatste rit van bst naar apt is om 19:37. Deze lijn zou klaar moeten zijn tussen de 19:58 en 20:00.  
+Lijn 401 eerste rit vertrekt om 6:04 vanuit de airport (Apt) naar Eindhoven Centraal (bst). 
+401 laatste rit van Apt naar bst is om 00:31. Deze lijn zou klaar moeten zijn tussen de 00:53 en 00:56
+Lijn 401 eerste rit vertrekt om 5:07 vanuit Eindhoven Centraal (bst) naar airport (Apt). 
+401 laatste rit van bst naar apt is om 00:09. Deze lijn zou klaar moeten zijn tussen de 00:31 en 00:33. 
+Lijn 400 eerste rit vertrekt om 7:19 vanuit de airport (Apt) naar Eindhoven Centraal (bst). 
+400 laatste rit van Apt naar bst is om 20:46. Deze lijn zou klaar moeten zijn tussen de 21:07 en 21:09 
+Lijn 400 eerste rit vertrekt om 06:52 vanuit Eindhoven Centraal (bst) naar airport (Apt). 
+401 laatste rit van bst naar apt is om 19:37. Deze lijn zou klaar moeten zijn tussen de 19:58 en 20:00.  
 """
